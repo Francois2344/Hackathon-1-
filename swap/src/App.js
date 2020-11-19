e2a6b99b2b089a73441d9ee700aee77bff45066f
loicbdev <loicbodolec@msn.com>
2020-11-19 14:33:58 +0100

    création SwapItem

--- swap/src/App.js
+++ swap/src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import SwapItem from './components/SwapItem';
 
 function App() {
   return (
     <div className="App">
         <h1>hello!</h1>
+        <SwapItem name = 'Wine'
+              image = 'https://www.vins-des-hospices-de-strasbourg.fr/data/uploads/2018/04/All%C3%A9e2.jpg'
+              category= 'Drink'
+              location ="sheerwood"
+              quantity = '10'
+              id ='1'/>
     </div>
   );
 }
