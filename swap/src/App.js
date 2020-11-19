1c903e67c5659c435eb9c6ac593238d74c5bffd7
loicbdev <loicbodolec@msn.com>
2020-11-19 16:28:21 +0100

    dev

--- swap/src/App.js
+++ swap/src/App.js
@@ -1,23 +1,15 @@
-<<<<<<< HEAD
-
-
-=======
 import './App.css';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import HomePage from './components/HomePage';
 import SwapList from './components/SwapList';
 import Deposit from './components/Deposit';
 import Account from './components/Account';
->>>>>>> c1f1ec0cfa517c9b503f24ec76feb7c62c5a595d
 
 function App() {
   return (
     <Router>
       
     <div className="App">
-<<<<<<< HEAD
-
-=======
     <Switch>
       <Route exact path='/'>
         <HomePage />
@@ -32,7 +24,6 @@ function App() {
         <Account />
       </Route>
       </Switch>
->>>>>>> c1f1ec0cfa517c9b503f24ec76feb7c62c5a595d
     </div>
     </Router>
     
