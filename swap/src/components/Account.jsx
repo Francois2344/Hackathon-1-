6a1c702cd6a5a7f2fdbdf828993a774a5d68fb64
NIcolasBonamy <n.bonamy@live.fr>
2020-11-19 20:05:06 +0100

    add style at account page

--- swap/src/components/Account.jsx
+++ swap/src/components/Account.jsx
@@ -1,19 +1,68 @@
 import { Component } from "react";
 import styled from "styled-components";
+import knight from "./pictos/Knight.svg"
 
 const Main = styled.div`
-
+  background-color: rgb(203, 57, 53);
+  margin: 8rem 1rem 5rem 1rem;
+  form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    img {
+        width: 200px;
+        height: 200px;
+        object-fit: cover;
+        object-position: top;
+        margin: 2rem 0 2rem 0;
+        background-color: #ffffff;
+    }
+    label {
+        width: 80%;
+        margin: 1rem;
+        input {
+            font-size: 20px;
+            padding: 0 0 0 10px;
+            height: 2rem;
+        }
+        input:disabled {
+            font-size: 20px;
+            padding: 0 0 0 10px;
+            background-color: #CB4335;
+            border-style: solid;
+            border-color: #ffffff;
+            border-width: 0.5px;
+            color: #ffffff;
+        }
+    }
+    fieldset {
+        width: 80%;
+        margin: 1rem;
+        color: #ffffff;
+        border: 1px solid;
+    }
+    button {
+        width: 150px;
+        height: 50px;
+        font-size: 15px;
+        margin: 1rem;
+        background-color: #000000;
+        color: #ffffff;
+        border: 1px solid;
+        border-color: #000000;
+    }
+  }
 `;
 
 class Account extends Component {
   constructor(props) {
     super(props);
-    const firstName = localStorage.getItem('firstName')
-    const lastName = localStorage.getItem('lastName')
+    const firstName = localStorage.getItem("firstName");
+    const lastName = localStorage.getItem("lastName");
     this.state = {
       formEnabled: false,
-      firstName: firstName ? firstName : 'firstName',
-      lastName: lastName ? lastName : 'lastName',
+      firstName: firstName ? firstName : "firstName",
+      lastName: lastName ? lastName : "lastName",
     };
     this.handleClick = this.handleClick.bind(this);
     this.handleChangeFirstName = this.handleChangeFirstName.bind(this);
@@ -21,10 +70,10 @@ class Account extends Component {
   }
 
   handleClick(event) {
-      const { firstName, lastName } = this.state;
+    const { firstName, lastName } = this.state;
     event.preventDefault();
-    localStorage.setItem('firstName', firstName)
-    localStorage.setItem('lastName', lastName)
+    localStorage.setItem("firstName", firstName);
+    localStorage.setItem("lastName", lastName);
     this.setState({
       formEnabled: !this.state.formEnabled,
     });
@@ -49,9 +98,9 @@ class Account extends Component {
       localStorage.setItem("longitude", position.coords.longitude);
     });
     return (
-      <div className="Account">
+      <Main className="Account">
         <form onSubmit={this.handleClick}>
-          <img src="https://via.placeholder.com/200x120" alt="" />
+          <img src={knight} alt="account avatar" />
           <label htmlFor="firstName">
             <input
               disabled={!formEnabled}
@@ -64,8 +113,8 @@ class Account extends Component {
           </label>
           <label htmlFor="lastName">
             <input
-            disabled={!formEnabled}
-            value={lastName}
+              disabled={!formEnabled}
+              value={lastName}
               type="text"
               onChange={this.handleChangeLastName}
               id="lastName"
@@ -77,14 +126,11 @@ class Account extends Component {
             <div>latitude : {localStorage.getItem("latitude")}</div>
             <div>longitude : {localStorage.getItem("longitude")}</div>
           </fieldset>
-          <button
-            type="button"
-            onClick={this.handleClick}
-          >
-            {formEnabled ? 'Enregistrer' : 'Modifier'}
+          <button type="button" onClick={this.handleClick}>
+            {formEnabled ? "Enregistrer" : "Modifier"}
           </button>
         </form>
-      </div>
+      </Main>
     );
   }
 }
